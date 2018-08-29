e8a0b89e821372dc758722589266e044b5d5ed34
CountChu <visualge@gmail.com>
2018-08-29 23:08:37 +0800

    - Add MotionDetector class.

--- Test11.py
+++ Test11.py
@@ -5,6 +5,44 @@
 import cv2
 import numpy as np
 
+class MotionDetector:
+
+    cam = None
+    t_minus = None
+    t = None
+    t_plus = None
+
+    def __init__ (self, cam):
+        self.cam = cam
+
+    def readFirstFrame(self):    
+        self.t_minus = cv2.cvtColor(self.cam.read()[1], cv2.COLOR_RGB2GRAY)
+        self.t = cv2.cvtColor(self.cam.read()[1], cv2.COLOR_RGB2GRAY)
+        self.t_plus = cv2.cvtColor(self.cam.read()[1], cv2.COLOR_RGB2GRAY)
+
+    diffImage = None    
+
+    def isMotion(self):
+        self.diffImage = calculateDiffImg(self.t_minus, self.t, self.t_plus) 
+
+
+        unique, counts = np.unique(self.diffImage, return_counts=True)
+        d = dict(zip(unique, counts))
+        #print (d)
+        sum = d[0] + d[1] + d[2] + d[3]
+        print (sum)
+
+        res = True
+        if sum >= 800000:
+            res = False
+
+        return res    
+
+    def readNextFrame(self):
+        self.t_minus = self.t
+        self.t = self.t_plus
+        self.t_plus = cv2.cvtColor(self.cam.read()[1], cv2.COLOR_RGB2GRAY)
+
 def calculateDiffImg(t0, t1, t2):
     d1 = cv2.absdiff(t2, t1)
     d2 = cv2.absdiff(t1, t0)
@@ -15,42 +53,27 @@ cam = cv2.VideoCapture(0)
 winName = "Movement Indicator"
 cv2.namedWindow(winName)
 
-# Read three images first:
-t_minus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-t = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+
+md = MotionDetector(cam)
+md.readFirstFrame()
 
 while True:
 
-    diffImage = calculateDiffImg(t_minus, t, t_plus) 
 
-
-    unique, counts = np.unique(diffImage, return_counts=True)
-    d = dict(zip(unique, counts))
-    #print (d)
-    sum = d[0] + d[1] + d[2] + d[3]
-    print (sum)
-
-    isStatic = False
-    if sum >= 800000:
-        isStatic = True
-
-    if isStatic:    
+    if not md.isMotion():    
         cv2.putText (
-            diffImage, 
+            md.diffImage, 
             'STATIC',
-            (10, diffImage.shape[0] - 10), 
+            (10, md.diffImage.shape[0] - 10), 
             cv2.FONT_HERSHEY_SIMPLEX, 
             10, 
             (125, 125, 125), 
             4)   
 
-    cv2.imshow(winName, diffImage)
+    cv2.imshow(winName, md.diffImage)
 
     # Read next image
-    t_minus = t
-    t = t_plus
-    t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+    md.readNextFrame()
 
     key = cv2.waitKey(10)
     if key == 27:                       # ESC
