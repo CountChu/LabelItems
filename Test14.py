5a9d5a22dbf27853fd6bbc06663de65235f4e776
CountChu <visualge@gmail.com>
2018-09-02 00:31:12 +0800

    - Use subplots

--- Test14.py
+++ Test14.py
@@ -6,20 +6,29 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('sudoku.jpg')
-rows,cols,ch = img.shape
-pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
-pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
+def main():
 
-M = cv2.getPerspectiveTransform(pts1,pts2)
-dst = cv2.warpPerspective(img,M,(300,300))
+	img = cv2.imread('sudoku.jpg')
+	rows,cols,ch = img.shape
+	pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
+	pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
 
-plt.subplot(121)
-plt.imshow(img)
-plt.title('Input')
+	M = cv2.getPerspectiveTransform(pts1,pts2)
+	dst = cv2.warpPerspective(img,M,(300,300))
 
-plt.subplot(122)
-plt.imshow(dst)
-plt.title('Output')
+	fig, axes = plt.subplots(
+					ncols=2,
+					nrows=1)
+	ax0, ax1 = axes.flat
 
-plt.show()
+	ax0.imshow(img)
+	ax0.set_title('Input')
+
+	ax1.imshow(dst)
+	ax1.set_title('Output')
+
+	plt.show()
+
+
+if __name__ == '__main__':
+    main()    
