c773a95b55a501123ad1bb2d31728b50b7a9a96a
zhu guangyu <zhugy2@lenovo.com>
2018-09-03 10:14:21 +0800

    - Use space indents instead of tab.

--- Test12-transform.py
+++ Test12-transform.py
@@ -10,48 +10,48 @@ def main():
 
 	img = cv2.imread('sudoku.jpg')
 	rows,cols,ch = img.shape
-	pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
-	pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
+    pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
+    pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
 
-	M = cv2.getPerspectiveTransform(pts1,pts2)
-	dst = cv2.warpPerspective(img,M,(300,300))
+    M = cv2.getPerspectiveTransform(pts1,pts2)
+    dst = cv2.warpPerspective(img,M,(300,300))
 
-	fig, axes = plt.subplots(
-					ncols=3,
-					nrows=1)
-	ax0, ax1, ax2 = axes.flat
+    fig, axes = plt.subplots(
+                    ncols=3,
+                    nrows=1)
+    ax0, ax1, ax2 = axes.flat
 
-	#
-	# ax0 - Input
-	#
+    #
+    # ax0 - Input
+    #
 
-	ax0.imshow(img)
-	ax0.set_title('Input')
+    ax0.imshow(img)
+    ax0.set_title('Input')
 
-	#
-	# ax1 - Process
-	#
+    #
+    # ax1 - Process
+    #
 
-	processedImage = img.copy()
-	
-	for pt in pts1:
-		ax1.scatter(pt[0], pt[1])
+    processedImage = img.copy()
+    
+    for pt in pts1:
+        ax1.scatter(pt[0], pt[1])
 
-	ax1.set_prop_cycle(None)
-	for pt in pts2:
-		ax1.scatter(pt[0], pt[1])
+    ax1.set_prop_cycle(None)
+    for pt in pts2:
+        ax1.scatter(pt[0], pt[1])
 
-	ax1.imshow(processedImage)
-	ax1.set_title('Process')
+    ax1.imshow(processedImage)
+    ax1.set_title('Process')
 
-	#
-	# ax2 - Output
-	#
+    #
+    # ax2 - Output
+    #
 
-	ax2.imshow(dst)
-	ax2.set_title('Output')
+    ax2.imshow(dst)
+    ax2.set_title('Output')
 
-	plt.show()
+    plt.show()
 
 if __name__ == '__main__':
     main()    
